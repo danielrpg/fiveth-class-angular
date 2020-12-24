63a833dc22f50244ccf1a45b28d08eaa2766d25c
Daniel Fernandez <daniel.fernandezgp@gmail.com>
2020-12-24 08:59:18 -0400

    Adding more Component interactions like ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   pageTitle = 'Interaccion entre componentes';
   imgUrl = 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat-768x384.png';
   count: number = 0;
   name: string;
   userName: string;
   private _customerName: string;
+  @ViewChild('nameRef') nameElementRef: ElementRef;
+
+  ngAfterViewInit() {
+    this.nameElementRef.nativeElement.focus();
+    console.log(this.nameElementRef);
+  }
 
   get customerName(): string {
     return this._customerName;
