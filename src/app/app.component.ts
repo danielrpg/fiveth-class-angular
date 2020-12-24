344453a5e681374fa5bf477a320967574b0a623c
Daniel Fernandez <daniel.fernandezgp@gmail.com>
2020-12-23 23:10:01 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'component-interaction';
+  pageTitle = 'Interaccion entre componentes';
+  imgUrl = 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat-768x384.png';
+  count: number = 0;
+  name: string;
+  userName: string;
+  private _customerName: string;
+
+  get customerName(): string {
+    return this._customerName;
+  }
+
+  set customerName(value: string) {
+    this._customerName = value;
+    if(value === 'pablo') {
+      alert('pablito clavo un clavito en la calva de un calvito');
+    }
+  }
+  
+  increment() {
+    this.count += 1;
+  }
+
+  validateUserName(userName) {
+    this.userName = userName;
+    if(userName === 'gatito') {
+      alert('Miu Miu!!');
+    }
+  }
 }
